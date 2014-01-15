608955a328a4a992d77dff3568dba4c899b3c22a
Alex Buchholz <alex@getitllc.com>
2014-01-15 15:55:42 -0500

    First pass

--- action.js
+++ action.js
@@ -1,54 +1,75 @@
+var directionsDisplay; 
+var directionsService; 
+var stepDisplay;
+var markerArray = [];
+var map;
+var result;
+
 $(document).ready(function() {
 
-  /*
-  var csv_data;
-  $.get('data/data_names.csv', function(data) {
-    csv_data = data;
-  }, 'text');
-  var data_names = $.parse(csv_data,  userConfig());
-  */
+ directionsDisplay = new google.maps.DirectionsRenderer();
+  directionsService = new google.maps.DirectionsService();
+  function initialize()
+  {   
+    var mapProp = {
+      center:new google.maps.LatLng(38.889468, -77.03524),
+      zoom: 11,
+      mapTypeId:google.maps.MapTypeId.ROADMAP
+    };
+    map=new google.maps.Map(document.getElementById("googleMap"), mapProp);
+      
+  }
+  google.maps.event.addDomListener(window, 'load', initialize);    
 
-  var census_api_root = "http://api.bls.gov/publicAPI/v1/timeseries/data/";
-  var census_api_prefix = "CE";
-  var census_api_season = "U";
-  var census_api_indust = "08000000";
-  var census_api_data = "03";
-  var census_api_string = census_api_root + census_api_prefix + census_api_season + census_api_indust + census_api_data;
-
-/*
-  $.get(census_api_string, function(data) {
-    $('#main').text(JSON.stringify(json, undefined, 2));
+  $(document).on('click','#goButt', function(){
+    calcRoute();
+    directionsDisplay.setMap(map);
   });
-*/
-  var updatePage = function( resp ) {
-    $( '#temp').text( resp.length() );
-  };
-  var printError = function( req, status, err ) {
-    $( '#temp').text( err );
-    console.log( 'something went wrong', status, err );
-  };
-
-  var ajaxOptions = {
-    url: census_api_string,
-    contentType: 'text/plain',
-    dataType: 'jsonp',
-    success: updatePage,
-    error: printError
-  };
-
-  $.ajax(ajaxOptions);
-
-  //$('#temp').text('SOMETHING');
-
-/*
-
-    function userConfig() 
-    {
-        return {
-            delimiter: ",",
-            header: true,
-            dynamicTyping: true,
-        };
-    }
-*/
 });
+
+function calcRoute() {
+  // First, clear out any existing markerArray
+  // from previous calculations.
+  for (i = 0; i < markerArray.length; i++) {
+    markerArray[i].setMap(null);
+  }
+
+  var start = document.getElementById("startLocation").value;
+  var end = document.getElementById("endLocation").value;
+  var request = {
+    origin:start,
+    destination:end,
+    travelMode: google.maps.TravelMode.DRIVING
+  };
+  directionsService.route(request, function(result, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+      directionsDisplay.setDirections(result);
+      showSteps(result);
+    }
+  });
+}
+
+function showSteps(directionResult) {
+  // For each step, place a marker, and add the text to the marker's
+  // info window. Also attach the marker to an array so we
+  // can keep track of it and remove it when calculating new
+  // routes.
+  var myRoute = directionResult.routes[0].legs[0];
+
+  for (var i = 0; i < myRoute.steps.length; i++) {
+      var marker = new google.maps.Marker({
+        position: myRoute.steps[i].start_point,
+        map: map
+      });
+      console.log(marker.position);
+      attachInstructionText(marker, myRoute.steps[i].instructions);
+      markerArray[i] = marker;
+  }
+}
+
+function attachInstructionText(marker, text) {
+  google.maps.event.addListener(marker, 'click', function() {
+    stepDisplay.setContent(text);
+    stepDisplay.open(map, marker);
+  });
+}
